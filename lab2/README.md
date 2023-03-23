f71f47ff62404a35c4135fe4fa4e1fd260b5fad3
Aleksey <alekseykrazhev@yandex.by>
2023-03-23 23:30:11 +0300

    lab 2 finished

--- lab2/README.md
+++ lab2/README.md
@@ -1,1 +1,13 @@
 # Lab 2 - File color info application
+
+## [DockerHub link](https://hub.docker.com/r/alekseykrazhev/lab2-color-info)
+
+### Build with
+
+    - Python 3.10
+    - Flask
+    - Pillow
+
+### Start working with the project
+
+    * Open project in ```PyCharm``` or any othe IDE
--- lab2/app.py
+++ lab2/app.py
@@ -1,12 +1,12 @@
 import logging
 import os
 
-from PIL import Image
-from flask import Flask, render_template, redirect, url_for
+from PIL import Image, JpegImagePlugin
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 logging.basicConfig(filename='backend.log', level=logging.DEBUG)
-folder = '/home/alex/Downloads/ДляпроверкиLab2'
+folder = os.path.abspath('pictures/ДляпроверкиLab2')
 
 
 def get_image_info(filepath):
@@ -42,6 +42,7 @@ def get_image_info(filepath):
 
 def get_images_info(folder):
     images_info = []
+    logging.debug(f'PATH | {folder}')
     for filename in os.listdir(folder):
         filepath = os.path.join(folder, filename)
         if os.path.isfile(filepath):
@@ -56,28 +57,40 @@ def get_images_info(folder):
 @app.route('/')
 def index():
     info_res = get_images_info(folder)
-    return render_template('index.html', info_list=info_res), 200
+    folder_size = round(sum(d.stat().st_size for d in os.scandir(folder) if d.is_file()) / 1024 ** 2, 2)
+    return render_template('index.html', info_list=info_res, size=folder_size), 200
 
 
 @app.route('/default')
 def default():
     global folder
-    folder = '/home/alex/Downloads/ДляпроверкиLab2'
-    return redirect(url_for('index')), 308
+    folder = os.path.abspath('pictures/ДляпроверкиLab2')
+    logging.debug(f'PATH | {folder}')
+    return redirect(url_for('index'))
 
 
 @app.route('/peliculas')
 def peliculas():
     global folder
-    folder = '/home/alex/Downloads/peliculas-animadas'
-    return redirect(url_for('index')), 308
+    folder = os.path.abspath('pictures/peliculas-animadas')
+    logging.debug(f'PATH | {folder}')
+    return redirect(url_for('index'))
 
 
 @app.route('/png')
 def png():
     global folder
-    folder = '/home/alex/Downloads/png'
-    return redirect(url_for('index')), 308
+    folder = os.path.abspath('pictures/png')
+    logging.debug(f'PATH | {folder}')
+    return redirect(url_for('index'))
+
+
+@app.route('/custom', methods=['GET', 'POST'])
+def custom():
+    if request.method == 'GET':
+        return render_template('custom_folder.html')
+    if request.method == 'POST':
+        return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
